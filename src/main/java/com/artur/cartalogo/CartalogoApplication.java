39cdd85102dbe8ac68be84020e8b6f0910160bd5
Artur <arturjorgean@gmail.com>
2024-09-23 22:39:46 -0300

    config: arquivos iniciais do projeto adicionados.

---
+++ src/main/java/com/artur/cartalogo/CartalogoApplication.java
@@ -0,0 +1,13 @@
+package com.artur.cartalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartalogoApplication.class, args);
+	}
+
+}
